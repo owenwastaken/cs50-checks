de5e9e29f9fa912910316c768916e3381a98098b
Owen Ryan <owen@owenryan.us>
2024-02-08 15:28:15 -0500

    Add comments and log program output

--- secret-note/__init__.py
+++ secret-note/__init__.py
@@ -10,7 +10,12 @@ def doesnt_crash():
 def decrypt():
     """Decrypts secret note"""
     expected = "nevergonnagiveyouup!"
+
+    # Run program and log output so students can see what their program is doing
     actual = check50.run("python3 decrypt.py").stdout()
+    cs50.log(f"Note decrypted to "{actual}")
+
+    # Fail the check if the decrypted note is not the expected result
     if actual != expected:
         raise check50.Failure("Did not decrypt note correctly.", help="Make sure you are only printing your decrypted note")
 
