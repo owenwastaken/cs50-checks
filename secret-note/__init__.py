5930b502803aa17417972ccdf2bcf42d4f73dabb
Owen Ryan <owen@owenryan.us>
2024-02-09 12:18:31 -0500

    Fix bug: include decrypt.csv in check

--- secret-note/__init__.py
+++ secret-note/__init__.py
@@ -12,6 +12,9 @@ def decrypt():
     """Decrypts secret note"""
     expected_regex = r"nevergonnagiveyouup!\n?"
 
+    # Copy decrypt.csv into the program's working directory
+    check50.include("decrypt.csv")
+
     # Run program and log output so students can see what their program is doing
     actual = check50.run("python3 decrypt.py").stdout()
     check50.log(f'Note decrypted to "{actual}"')
