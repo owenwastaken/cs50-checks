c2744da7b33899dfaf9598dbe225d7d442a02119
Owen Ryan <owen@owenryan.us>
2024-02-09 12:19:40 -0500

    Include decrypt.csv for both checks

--- secret-note/__init__.py
+++ secret-note/__init__.py
@@ -4,6 +4,7 @@ import re
 @check50.check()
 def doesnt_crash():
     """decrypt.py doesn't throw an exception"""
+    check50.include("decrypt.csv")
     check50.run("python3 decrypt.py").exit(0)
 
 
